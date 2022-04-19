8dc82eab66fee1e7ac85f277a9fd4c0d6ec7b7ad
WakeMeUp <m.baglan01@gmail.com>
2022-04-19 15:56:09 +0600

    Header added

---
+++ src/components/Content/Content.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DownloadPart from "./DownloadPart";
+
+function Content(){
+    return(
+        <div className={`Content`}>
+
+            <div className="divider"></div>
+
+            <DownloadPart/>
+
+
+
+        </div>
+    )
+}
+
+export default Content();
---
+++ src/components/Content/DownloadPart.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function DownloadPart(){
+    return(
+        <div className={`DownloadPart`}>
+
+            <div className={`fileLoadingPartWrapper`}>
+
+                <div className="fileLoadingPart">
+
+                    <div className="fileLoadingPartTitle">
+                        File Load
+                    </div>
+
+
+                    <div className="fileLoadingPartBody">
+                        <div className="upload-container">
+
+                            <div id="upload_icon" className="upload_icon">
+
+                            </div>
+
+                            <div id="loaded_data" className="loaded_data">
+
+                                <div className="loaded_data_icon">
+
+
+                                </div>
+
+                                <div id='loaded_data_text' className="loaded_data_text">
+
+
+                                    <input type="file" id="file_upload" accept="audio/*"/>
+
+                                    <div className="upload_text">
+                                        Click (or) Drag and Drop files here.
+                                    </div>
+
+
+                                </div>
+
+
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default DownloadPart();
