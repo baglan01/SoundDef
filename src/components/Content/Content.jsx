b61fe331120924b3c531d1747b28d67aa7b69ab0
WakeMeUp <m.baglan01@gmail.com>
2022-04-25 23:53:24 +0600

    added maket of main animation

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DownloadPart from "./DownloadPart";
 import Divider from "../SubComponents/Divider";
+import Main from "./Main";
 
 function Content(){
     return(
         <div className={`Content`}>
+            <Main/>
             <Divider/>
             <DownloadPart/>
         </div>
---
+++ src/components/Content/Main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Main(){
+
+    
+
+    return(
+        <div className="mainInfo">
+
+            <div className="mainInfoSloganWrap">
+                <div className="mainInfoSlogan">
+                    Sound is everywhere
+                </div>
+
+
+            </div>
+
+
+            <div className="mainInfoAnimation" id="mainInfoAnimation">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Main;
